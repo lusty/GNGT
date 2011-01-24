1926d08f098465a95ca82b74a19869bc342d2b30
Richard Clark <rdclark@nextquestion.net>
2011-01-24 15:29:16 -0800

    Added location to data model

--- db/buildGardenDB.py
+++ db/buildGardenDB.py
@@ -1,7 +1,20 @@
 import sqlite3;
 from datetime import datetime, date;
 import time
- 
+import json, urllib
+
+GEOCODE_BASE_URL = 'http://maps.googleapis.com/maps/api/geocode/json'
+
+def geocode(address,sensor, **geo_args):
+    geo_args.update({
+        'address': address,
+        'sensor': sensor  
+    })
+
+    url = GEOCODE_BASE_URL + '?' + urllib.urlencode(geo_args)
+    result = json.load(urllib.urlopen(url))
+    return result['results'][0]['geometry']['location']
+
 inConn = sqlite3.connect('gardenList.sqlite3')
 outConn = sqlite3.connect('GNGT.sqlite')
  
@@ -18,17 +31,23 @@ for row in inCursor:
         maxId = row[0]
  
     # Create ZGARDENINFO entry
+	address = "%(street)s, %(city)s, CA" % ({'street':row[4], 'city':row[5]})
+	loc = geocode(address, "false")
+    print "Inserting %s..." % (row[0])
+	
     vals = []
     vals.append(row[0]) # Z_PK
     vals.append(2) # Z_ENT
     vals.append(1) # Z_OPT
     vals.append(row[1]) # ZGARDENNUMBER
     vals.append(row[0]) # ZGARDENDESCRIPTION
-    vals.append(row[4]) # ZSTREET
+    vals.append(loc['lat']) # ZLATITUDE
+    vals.append(loc['lng']) # ZLONGITUDE
+    vals.append(row[5]) # ZCITY
     vals.append(row[2]) # ZGARDENNAME
     vals.append(row[6]) # ZPLANTSALE
-    vals.append(row[5]) # ZCITY
-    outConn.execute("insert into ZGARDENINFO values(?, ?, ?, ?, ?, ?, ?, ?, ?)", vals)
+    vals.append(row[4]) # ZSTREET
+    outConn.execute("insert into ZGARDENINFO values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", vals)
  
     # Create ZGARDENDESCRIPTION entry
     vals = []
