6052d2e06dfb386c2292bd5a798fb3e0696546bd
Richard Clark <rdclark@nextquestion.net>
2011-01-25 07:59:26 -0800

    Start of work on favorites

---
+++ Classes/Checkbox.h
@@ -0,0 +1,26 @@
+//
+//  Checkbox.h
+//  MyPractice
+//
+//  Created by Richard Clark on 10/17/08.
+//  Copyright 2008 NextQuestion Consulting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface Checkbox : UIControl {
+	BOOL on;	
+	UIImage *checkmark;
+	
+@private
+	BOOL tracking;
+}
+
+@property BOOL on;
+
+@property (retain) UIImage *checkmark;
+
+#define CHECKBOX_SIZE 44.0f
+
+@end
