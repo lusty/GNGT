a2dbdb2c729a1981ab00e6979b8d0a5b6031c006
Richard Clark <rdclark@nextquestion.net>
2011-02-11 13:50:06 -0800

    Stub for JSON importer

---
+++ Classes/ImportJSON.h
@@ -0,0 +1,16 @@
+//
+//  ImportJSON.h
+//  GNGT
+//
+//  Created by Richard Clark on 2/11/11.
+//  Copyright 2011 NextQuestion Consulting. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface ImportJSON : NSObject {
+
+}
+
+@end
---
+++ Classes/ImportJSON.m
@@ -0,0 +1,14 @@
+//
+//  ImportJSON.m
+//  GNGT
+//
+//  Created by Richard Clark on 2/11/11.
+//  Copyright 2011 NextQuestion Consulting. All rights reserved.
+//
+
+#import "ImportJSON.h"
+
+
+@implementation ImportJSON
+
+@end
